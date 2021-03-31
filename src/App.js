d9b2a1e4610979a69e54c4b4e358a87fc9ef1bf0
Inan Xu <ixu2002new@gmail.com>
2021-03-31 01:08:20 -0700

    add react-graph-vis & react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,58 +1,133 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
+import './App.css';
 import footerStyles from './styles/footer.module.css';
 import headerStyles from './styles/header.module.css';
 
-function Header(props) {
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import GraphVis from 'react-graph-vis';
+
+function SubmitButton(props) {
+  var toValue = `${props.url}/${props.maxNodes}`
+  return (
+    <Router>
+      <button className="input" type="button" to={toValue}>Crawl</button>
+    </Router>
+  );
+}
+const Header = () => {
   return (
     <header>
       <h1 className={headerStyles.title}>
         <a href="https://ixukw.github.io">ixukw</a>
       </h1>
-      WebCrawler Version 2
+      WebCrawler
     </header>
   );
 }
-function Footer(props) {
+
+const Footer = () => {
   return <footer className={footerStyles.footer}>ixukw</footer>;
 }
-class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-  render() {
-    return (
-      <div className="input">
-        <p>{this.props.name}: {this.state.value}</p>
-        <input type={this.props.type} className={this.props.type} min="0" max={this.props.max} value={this.state.value} onChange={this.handleChange}/>
-      </div>
-    );
-  }
-}
-function App() {
+const Input = () => {
+  const [value, setValue] = useState(0);
   return (
-    <div className="container">
+    <div className="input">
+      <p>{this.props.name}: {this.state.value}</p>
+      <input type={this.props.type} className={this.props.type} min="0" max={this.props.max} value={this.state.value} onChange={this.handleChange}/>
+    </div>
+  );
+  
+}
+const Graph = () => {
+  const graph = {
+    nodes: [
+      { id: 1, label: "Node 1", title: "node 1 tootip text" },
+      { id: 2, label: "Node 2", title: "node 2 tootip text" },
+      { id: 3, label: "Node 3", title: "node 3 tootip text" },
+      { id: 4, label: "Node 4", title: "node 4 tootip text" },
+      { id: 5, label: "Node 5", title: "node 5 tootip text" }
+    ],
+    edges: [
+      { from: 1, to: 2 },
+      { from: 1, to: 3 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 }
+    ]
+  };
+  const options = {
+    layout: {
+      hierarchical: true
+    },
+    edges: {
+      color: "#000000"
+    },
+    height: "500px"
+  };
+
+  const events = {
+    select: function(event) {
+      var { nodes, edges } = event;
+    }
+  };
+  return (
+    <GraphVis
+      graph={graph}
+      options={options}
+      events={events}
+      getNetwork={network => {
+        //  if you want access to vis.js network api you can set the state in a parent component using this property
+      }}
+    />
+  );
+}
+const App = () => {
+  return (
+    <Container>
+      <Row>
+        <Header />
+      </Row>
+      <Row>
+        <Col>
+          Input
+        </Col>
+        <Col>
+          Graph
+          <Graph/>
+        </Col>
+      </Row>
+      <Row className="fixed-bottom">
+        <Footer />
+      </Row>
+    </Container>
+    /*<div className="container">
       <Header />
       <main className="main">
-        <div className="form">
-          <Input name="URL" type="text" />
-          <Input name="Maximum Nodes" max="10" type="number" />
+        <div className="left">
+          <div className="form">
+            <Input name="URL" type="text" />
+            <Input name="Maximum Nodes" max="10" type="number" />
+            
+          </div>
         </div>
-
-        <div className="graph">
-          Graph
-          <div id="test"></div>
+        <div className="right">
+          <div className="graph">
+            Graph
+          </div>
         </div>
       </main>
       <Footer />
-    </div>
+          </div>*/
   );
 }
 
